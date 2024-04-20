73d8cc0bd854d8cf357f8552aa9478a6fa9687b1
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2024-04-20 12:22:04 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+          
+        private void SetUpGame()
+        {
+            // Create a List of animals emoji
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐙","🐙",
+                "🐟","🐟",
+                "🐘","🐘",
+                "🐺","🐺",
+                "🐫","🐫",
+                "🐱‍🐉","🐱‍🐉",
+                "🐯","🐯",
+                "🦔","🦔",
+            };
+
+            // Create a new random number generator
+            Random random = new Random();
+
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count); // Pick a random number between 0 and number of the elements in the list
+                string nextEmoji = animalEmoji[index]; // Retrieve emoji with given index from the emoji List
+                textBlock.Text = nextEmoji; // Update textBlock text with random emoji
+                animalEmoji.RemoveAt(index); // Remove randomly picke emoji
+            }
+        }
+    }
+}
